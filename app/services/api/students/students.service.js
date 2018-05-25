18a4601b869ca54893c0372b3f152cb9db20fc52
Adriana Rubi <rubi12wera@gmail.com>
2018-05-25 17:21:12 -0600

    ng-interview project solution

--- app/services/api/students/students.service.js
+++ app/services/api/students/students.service.js
@@ -5,21 +5,37 @@
 		.module('ngInterview.api.students')
 		.service('StudentsService', StudentsService);
 
-	StudentsService.$inject = [];
-	function StudentsService() {
+	StudentsService.$inject = ['$http', '$timeout'];
+	function StudentsService($http, $timeout) {
 
 		/**
 		 * Exposed functions
 		 */
 
 		this.getName = getName; // This function serves no purpose. It's just here as an example.
-
+		
 		/**
 		 * Implementations
 		 */
+		var triedTimes = 0;
+
+		this.getStudentList = function () {
+			triedTimes ++;
+		 	return $http.get('http://il-resume-api.azurewebsites.net/api/students')
+                .then(function(response){
+                	return response;
+                }, function(response){
+                	return response;
+                });
+                
+		 };
+		var reTry = this.getStudentList;
+		 
+
+
 
 		function getName() {
 			return 'studentsService';
-		}
+		};
 	}
 })();
--- app/students/students.controller.js
+++ app/students/students.controller.js
@@ -5,15 +5,18 @@
 		.module('ngInterview.students')
 		.controller('StudentsController', StudentsController);
 
-	StudentsController.$inject = [];
-	function StudentsController() {
+	StudentsController.$inject = ['StudentsService'];
+	function StudentsController(StudentsService) {
 
 		/**
 		 * Model
 		 */
 
 		var vm = this;
-
+			vm.students = [];
+			vm.errorMessage;
+		var triedTimes= 0;
+		var errorMessageText = "Student data could not be loaded at this time, please reload page or try again later."
 		/**
 		 * Initialization
 		 */
@@ -26,6 +29,44 @@
 
 		function activate() {
 			// Initialization code goes here
+			
+		getStudentsData();
+		}
+
+		function getStudentsData() {
+			triedTimes ++;
+			var studentData= StudentsService.getStudentList().then(function (response){
+				if (response && response.data && typeof response.data === "object"){
+					try{
+					// checking for invalid JSON
+						var respText = JSON.stringify(response.data);
+						try{
+							var jsonObject = JSON.parse(respText);
+							vm.students = jsonObject;
+						}
+						catch(e){
+				 			vm.errorMessage = errorMessageText;
+						}
+					}
+					catch(e){
+						vm.errorMessage = errorMessageText;
+					}
+				}
+				else{
+					if(triedTimes < 2){
+						getStudentsData();
+					}
+					else{
+						vm.errorMessage = errorMessageText;
+					}
+				}
+				
+
+			}, function (response){
+				vm.errorMessage = errorMessageText;
+			}); 
+
+			
 		}
 	}
 })();
